e88ace57b441df293c188d5b5a47b4bf64c67402
radpaolo <PaoloSambrano@gmail.com>
2017-08-16 11:16:29 -0700

    i can make and shuffle deck  Please enter the commit message for your changes. Lines starting

--- blackjack.js
+++ blackjack.js
@@ -5,78 +5,31 @@ class Card{
   }
 }
 
-class Rank{
-  constructor(){
-  }
-}
-
-class Suit{
-  constructor(){
-}
-}
-
-var deck = [];
-
 function getDeck(){
   var deck = [];
-  deck.push(twoSpades, threeSpades, fourSpades, fiveSpades, sixSpades, sevenSpades);
+  var suits =["hearts", "diamonds", "clubs", "spades"];
+  var ranks = ["2", "3", "4", "5", "6", "7", "8","9","10", "J", "Q", "K", "A"];
+  for (var i = 0; i < suits.length; i++){
+  for (var x = 0; x < ranks.length; x++){
+    var card = new Card (ranks[x], suits[i]);
+    deck.push(card);
+  }
+  }
   return deck;
 }
 
-var twoSpades = new Card(2, 'Spades');
-var threeSpades = new Card(3, 'Spades');
-var fourSpades = new Card(4, 'Spades');
-var fiveSpades = new Card(5, 'Spades');
-var sixSpades = new Card(6, 'Spades');
-var sevenSpades = new Card(7, 'Spades');
-var eightSpades = new Card(8, 'Spades');
-var nineSpades = new Card(9, 'Spades');
-var tenSpades = new Card(10, 'Spades');
-var jackSpades = new Card(10, 'Spades');
-var queenSpades = new Card(10, 'Spades');
-var kingSpades = new Card(10, 'Spades');
-var aceSpades = new Card(11, 'Spades');
+function shuffleDeck(deck){
+  for (var i = 0; i < 1000; i++) {
+    var location1 = Math.floor ((Math.random () * deck.length));
+    var location2 = Math.floor ((Math.random () * deck.length));
+    var temp = deck[location1];
 
-var twoDiamonds = new Card(2, 'Diamonds');
-var threeDiamonds = new Card(3, 'Diamonds');
-var fourDiamonds = new Card(4, 'Diamonds');
-var fiveDiamonds = new Card(5, 'Diamonds');
-var sixDiamonds = new Card(6, 'Diamonds');
-var sevenDiamonds = new Card(7, 'Diamonds');
-var eightDiamonds = new Card(8, 'Diamonds');
-var nineDiamonds = new Card(9, 'Diamonds');
-var tenDiamonds = new Card(10, 'Diamonds');
-var jackDiamonds = new Card(10, 'Diamonds');
-var queenDimonds = new Card(10, 'Diamonds');
-var kingDiamonds = new Card(10, 'Diamonds');
-var aceDiamonds = new Card(11, 'Diamonds');
+    deck[location1] = deck[location2];
+    deck[location2] = temp;
+  }
+  return deck;
+}
 
-var twoHearts = new Card(2, 'Hearts');
-var threeHearts = new Card(3, 'Hearts');
-var fourHearts = new Card(4, 'Hearts');
-var fiveHearts = new Card(5, 'Hearts');
-var sixHearts = new Card(6, 'Hearts');
-var sevenHearts = new Card(7, 'Hearts');
-var eightHearts = new Card(8, 'Hearts');
-var nineHearts = new Card(9, 'Hearts');
-var tenHearts = new Card(10, 'Hearts');
-var jackHearts = new Card(10, 'Hearts');
-var queenHearts = new Card(10, 'Hearts');
-var kingHearts = new Card(10, 'Hearts');
-var aceHearts = new Card(11, 'Hearts');
-
-var twoClubs = new Card(2, 'Clubs');
-var threeClubs = new Card(3, 'Clubs');
-var fourClubs = new Card(4, 'Clubs');
-var fiveClubs = new Card(5, 'Clubs');
-var sixClubs = new Card(6, 'Clubs');
-var sevenClubs = new Card(7, 'Clubs');
-var eightClubs = new Card(8, 'Clubs');
-var nineClubs = new Card(9, 'Clubs');
-var tenClubs = new Card(10, 'Clubs');
-var jackClubs = new Card(10, 'Clubs');
-var queenClubs = new Card(10, 'Clubs');
-var kingClubs = new Card(10, 'Clubs');
-var aceClubs = new Card(11, 'Clubs');
-
-console.log();
+var deck = getDeck();
+deck = shuffleDeck(deck);
+console.log(deck.length);
